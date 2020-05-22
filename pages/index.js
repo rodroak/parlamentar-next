159e8f7d2bb18fcd1f7f9d8b165b8d7d57fd3d0f
rodroak <rodrigocc78646@gmail.com>
2020-05-22 01:00:51 +0100

    followed next basic docs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div className="welcome">
+      <img className="welcome__logo" src="/logo.svg" alt="" />
+      <p className="welcome__msg">Welcome to Parlamentar!</p>
+      <ul className="welcome__ul">
+        <li className="welcome__li">
+          <Link href="#">
+            <a className="btn">Let's go</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a className="btn">About us</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default HomePage;
