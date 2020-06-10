c94bec819db29b180b48f1d37b2bd6865eef76ba
rodroak <rodrigocc78646@gmail.com>
2020-06-10 23:46:59 +0100

    range input css done

--- pages/partidos.js
+++ pages/partidos.js
@@ -1,5 +1,6 @@
-import Header from "../components/Header";
 import Link from "next/link";
+import { detect } from "detect-browser";
+import Header from "../components/Header";
 
 const colorPS = "#f733b6";
 
@@ -29,97 +30,143 @@ const partyPosition = [
   },
 ];
 
-const PartidosPage = () => {
-  return (
-    <div className="page-wrapper">
-      <Header />
-      <div className="content content-party">
-        <img className="party__logo" src="/partidos/PS.png" alt="logo PS" />
-        <Link href="#">
-          <a className="btn party__btn">&#43;</a>
-        </Link>
-        <h1 className="party__name">Partido Socialista</h1>
-        <ul className="party__data">
-          {partyData.map((item) => {
-            const gradientPercent = (item.value / item.total) * 100;
-            return (
-              <li className="party__data-item" key={item.title}>
-                <p className="party__data-item-title">{item.title}</p>
-                <div
-                  className="party__data-item-bar"
-                  style={{
-                    background: `linear-gradient(90deg, ${colorPS} 0%, ${colorPS} ${gradientPercent}%, white ${gradientPercent}%, white 100%)`,
-                  }}
-                >
-                  <p className="party__data-item-value">
-                    {item.value} / {item.total}
-                  </p>
+class PartidosPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      year: "1986",
+      isChrome: false,
+    };
+  }
+
+  componentDidMount() {
+    const browser = detect();
+    this.setState({
+      isChrome: browser.name === "chrome",
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({ year: event.target.value });
+  };
+
+  render() {
+    const newValue = Number(((this.state.year - 1976) * 100) / (2020 - 1976));
+    const newPosition = 11 - newValue * 0.26;
+    const { isChrome } = this.state;
+    return (
+      <div className="page-wrapper">
+        <Header />
+        <div className="content content-party">
+          <img className="party__logo" src="/partidos/PS.png" alt="logo PS" />
+          <Link href="#">
+            <a className="btn party__btn">&#43;</a>
+          </Link>
+          <h1 className="party__name">Partido Socialista</h1>
+          <ul className="party__data">
+            {partyData.map((item) => {
+              const gradientPercent = (item.value / item.total) * 100;
+              return (
+                <li className="party__data-item" key={item.title}>
+                  <p className="party__data-item-title">{item.title}</p>
+                  <div
+                    className="party__data-item-bar"
+                    style={{
+                      background: `linear-gradient(90deg, ${colorPS} 0%, ${colorPS} ${gradientPercent}%, white ${gradientPercent}%, white 100%)`,
+                    }}
+                  >
+                    <p className="party__data-item-value">
+                      {item.value} / {item.total}
+                    </p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+          <ul className="party__position">
+            {partyPosition.map((position) => (
+              <li className="party__position-item" key={position.position}>
+                <img
+                  src={position.src}
+                  alt={position.name}
+                  className="party__position-img"
+                />
+                <div className="party__position-details">
+                  <p className="party__position-name">{position.name}</p>
+                  <p className="party__position-title">{position.position}</p>
                 </div>
               </li>
-            );
-          })}
-        </ul>
-        <ul className="party__position">
-          {partyPosition.map((position) => (
-            <li className="party__position-item" key={position.position}>
-              <img
-                src={position.src}
-                alt={position.name}
-                className="party__position-img"
+            ))}
+          </ul>
+          <div className="party__range-container">
+            <p className="party__range-min">1976</p>
+            <div className="party__range-wrapper">
+              <input
+                className="party__range"
+                min="1976"
+                max="2020"
+                type="range"
+                value={this.state.year}
+                onChange={this.handleChange}
+                style={{ background: isChrome ? `linear-gradient(90deg, ${colorPS} 0%, ${colorPS} ${newValue}%, #a7a7a7 ${newValue}%, #a7a7a7 100%)` : "transparent" }}
               />
-              <div className="party__position-details">
-                <p className="party__position-name">{position.name}</p>
-                <p className="party__position-title">{position.position}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <input className="party__range" type="range" />
-        <div className="party__other-logos">
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/PSD.png"
-            alt="logo PS"
-          />
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/BE.png"
-            alt="logo PS"
-          />
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/PCP.png"
-            alt="logo PS"
-          />
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/CDS.png"
-            alt="logo PS"
-          />
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/PAN.png"
-            alt="logo PS"
-          />
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/PEV.png"
-            alt="logo PS"
-          />
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/CH.png"
-            alt="logo PS"
-          />
-          <img
-            className="party__other-logo hover-scale"
-            src="/partidos/IL.png"
-            alt="logo PS"
-          />
+              <p
+                className="party__range-value"
+                style={{
+                  left: `calc(${newValue}% + (${newPosition}px))`,
+                }}
+              >
+                {this.state.year}
+              </p>
+            </div>
+            <p className="party__range-max">2020</p>
+          </div>
+          <div className="party__other-logos">
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/PSD.png"
+              alt="logo PS"
+            />
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/BE.png"
+              alt="logo PS"
+            />
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/PCP.png"
+              alt="logo PS"
+            />
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/CDS.png"
+              alt="logo PS"
+            />
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/PAN.png"
+              alt="logo PS"
+            />
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/PEV.png"
+              alt="logo PS"
+            />
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/CH.png"
+              alt="logo PS"
+            />
+            <img
+              className="party__other-logo hover-scale"
+              src="/partidos/IL.png"
+              alt="logo PS"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PartidosPage;
